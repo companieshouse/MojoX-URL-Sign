3c304d6225e529502c7eeb743a3921bc36908ab8
Ben Vinnerd <ben@vinnerd.com>
2014-03-31 14:33:39 +0100

    Added POD

--- lib/MojoX/URL/Sign/Plugin.pm
+++ lib/MojoX/URL/Sign/Plugin.pm
@@ -18,3 +18,70 @@ sub register {
 }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+MojoX::URL::Sign::Plugin - <Mojolicious> plugin for L<MojoX::URL::Sign>
+
+=head1 SYNOPSIS
+
+    # Somewhere inside your L<Mojolicious> app C<startup>...
+    $self->plugin('MojoX::URL::Sign::Plugin', salt => 'secret-salt'); # Only need to pass the salt here
+    
+    # Somewhere inside your L<Mojolicious> controller...
+    my $signed_url = $self->sign_url('http://example.com/some_page?for=bar');
+    # (Defaults to current page if no URL arg passed in)
+    $self->render(text => "The signed URL is $signed_url");
+
+    # ...or inside your template...
+    <a href="<% $c.sign_url($c.url_for('some_page', foo => $bar).to_abs) %>">My link</a>
+
+    # Then in the "some_page" action...
+    my $verified_url = $self->verify_url; # verifies the current URL of this action
+    
+    unless ($verified_url) {
+        return $self->render_not_found;
+    }
+
+    $self->render(text => "$verified_url has been verified");
+
+=head1 DESCRIPTION
+
+L<MojoX::URL::Sign::Plugin> is a simple L<Mojolicious> plugin to expose L<MojoX::URL::Sign> C<sign_url>
+and C<verify_url> methods as helpers.
+
+The name of the helpers have the same name as the methods which they point to.
+
+See L<MojoX::URL::Sign> for detailed information.
+
+=head1 HELPER METHODS
+
+=head2 sign_url
+
+    my $signed_url = $self->sign_url($url_to_sign); # Sign a specifiec URL
+    my $signed_url = $self->sign_url;               # Sign the URL of the current controller/action
+
+=head2 verify_url
+
+    my $verified_url = $self->verify_url;
+
+If URL verified OK, then C<$verified_url> is the original URL without the signature.
+Otherwise it will be C<undef>
+
+=head1 CAVEATS
+
+=over 4
+
+=item * URL must be absolute, and with scheme and host
+
+If using L<Mojolicious::Controller/url_for>, ensure that you use L<-E<gt>to_abs|Mojo::URL/to_abs>
+
+=back
+
+=head1 AUTHOR
+
+Ben Vinnerd, C<ben@vinnerd.com>
+
+=cut
